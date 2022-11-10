7f13f92b5bda7972480b49aac0af3bc50bf7983d
artem <bccons@ya.ru>
2022-11-10 23:21:56 +0300

    Mock tests, add inputs

--- udemy/blog/app.py
+++ udemy/blog/app.py
@@ -1,3 +1,4 @@
+MENU_PROMPT = 'Input "c" to create a blog, "l" to list blogs, "r" to read one, "p" to create a post, "q" to quit: '
 blogs = dict()  #blog_name : Blog object
 
 def menu():
@@ -8,21 +9,8 @@ def menu():
     exit
     '''
     print_blogs()
-
+    selection = input(MENU_PROMPT)
 def print_blogs():
     for key, blog in blogs.items():
         print('- {}'.format(blog))
 blogs = dict()  #blog_name : Blog object
-
-def menu():
-    '''
-    show the user the available blogs
-    let the user make a choice
-    do something with that choice
-    exit
-    '''
-    print_blogs()
-
-def print_blogs():
-    for key, blog in blogs.items():
-        print('- {}'.format(blog))
--- udemy/blog/test/system/test_app.py
+++ udemy/blog/test/system/test_app.py
@@ -1,9 +1,21 @@
+import builtins
 from unittest import TestCase
 from unittest.mock import patch
 import app
 from blog import Blog
 
 class AppTest(TestCase):
+    def test_menu_prints_prompt(self):
+        with patch('builtins.input') as mocked_input:
+            app.menu()
+            mocked_input.asssert_called_with(app.MENU_PROMPT)
+
+    def test_menu_calls_print_blogs(self):
+        with patch('app.print_blogs') as mocked_print_blogs:
+            with patch('builtins.input', return_value='q'):
+                app.menu()
+                mocked_print_blogs.assert_called()
+
     def test_print_blogs(self):
         blog = Blog('Test', 'Test author')
         app.blogs = {'Test': blog}
