09196b20d44186759ed9e867cd6ffe69588fd048
artem <bccons@ya.ru>
2022-11-14 18:33:03 +0300

    Udemy section 4, Flask. End

--- udemy_4/app/tests/system/test_home.py
+++ udemy_4/app/tests/system/test_home.py
@@ -1,10 +1,17 @@
 from unittest import TestCase
 from app.app import app
+from app.tests.system.test_base import TestBase
 import json
 
 class TestHome(TestCase):
     def test_home(self):
         with app.test_client() as c:
+            resp = c.get('/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(json.loads(resp.get_data()), {'message': 'Hello world!'})
+class TestHome(TestBase):
+    def test_home(self):
+        with self.app as c:
             resp = c.get('/')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(json.loads(resp.get_data()), {'message': 'Hello world!'})
